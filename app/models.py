e46a044378821e88c15b165edfd87a4aad948a11
AwesomeAdil <84029606+AwesomeAdil@users.noreply.github.com>
2024-09-21 13:58:50 -0500

    auth

--- app/models.py
+++ app/models.py
@@ -1,8 +1,17 @@
-from app import db
+from app import mongo
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100))
-    condition = db.Column(db.String(100))
-    city = db.Column(db.String(100))
-    created_at = db.Column(db.DateTime, default=db.func.now())
+class User:
+    def __init__(self, name, condition, city):
+        self.name = name
+        self.condition = condition
+        self.city = city
+
+    def save(self):
+        if mongo.db:  # Check if MongoDB is initialized
+            mongo.db.users.insert_one({
+                'name': self.name,
+                'condition': self.condition,
+                'city': self.city
+            })
+        else:
+            print(f"Skipping save for {self.name}, MongoDB not initialized.")
